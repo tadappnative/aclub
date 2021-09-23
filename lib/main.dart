9f7e923307294bb9e7b8e2a047213106a60ea43b
tadappnative <kantemirova88@yandex.ru>
2021-09-23 18:40:17 +0300

    update id

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:aclub/screens/welcome_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+  OneSignal.shared.setAppId("52840c31-55ac-4c71-835c-1a46485599c1");
+  OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+    print("Accepted permission: $accepted");
+  });
   runApp(WebViewApp());
 }
 
